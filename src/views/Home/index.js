a97d9edb2615382599aebe00d72d77f6328fd841
el-zacharoo <zachary.weston@propellerhead.co.nz>
2021-03-29 10:40:59 +1300

    initial commit

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 import { Header } from '../../components/Layout/Header'
 import { Footer } from '../../components/Layout/Footer'
+
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -19,12 +21,21 @@ const useStyles = makeStyles(theme => ({
 
 const Business = () => {
     const classes = useStyles();
+    const [view, setView] = useState('false');
 
     return (
         <>
             <Header />
             <Box justifyContent="center" className={classes.root} >
-                <Button>Test</Button>
+                {view === ('false') &&
+                    <Button variant="contained" color="primary" onClick={() => setView('true')}>Open</Button>
+                }
+                {view === ('true') &&
+                    <>
+                        <Button onClick={() => setView('false')}>Close X</Button>
+                        <Typography>Test</Typography>
+                    </>
+                }
             </Box>
             <Footer />
         </>
