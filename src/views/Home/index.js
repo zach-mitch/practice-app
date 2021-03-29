bdcc6cf2d96b8cc837e83c330828bc614ab18883
el-zacharoo <zachary.weston@propellerhead.co.nz>
2021-03-29 13:55:50 +1300

    initial changes

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import { Home } from '../../components/Icons/Home';
 
 import { Header } from '../../components/Layout/Header'
 import { Footer } from '../../components/Layout/Footer'
@@ -16,6 +19,9 @@ const useStyles = makeStyles(theme => ({
         [theme.breakpoints.down('sm')]: {
             padding: theme.spacing(1),
         }
+    },
+    icon: {
+        color: theme.palette.secondary.main
     }
 }));
 
@@ -28,12 +34,16 @@ const Business = () => {
             <Header />
             <Box justifyContent="center" className={classes.root} >
                 {view === ('false') &&
-                    <Button variant="contained" color="primary" onClick={() => setView('true')}>Open</Button>
+                    <IconButton onClick={() => setView('true')}><Home className={classes.icon} /></IconButton>
+
+
                 }
                 {view === ('true') &&
                     <>
-                        <Button onClick={() => setView('false')}>Close X</Button>
-                        <Typography>Test</Typography>
+                        <Button endIcon={
+                            <CloseIcon className={classes.icon} />
+                        } onClick={() => setView('false')}>Close</Button>
+                        <Typography variant="h1">Test</Typography>
                     </>
                 }
                 <Typography>
