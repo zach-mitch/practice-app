b52444ceb11766073e1fade957969ee56c8b5a0e
@Mitch <mr.mitcholl@gmail.com>
2021-04-01 11:01:18 +1300

    added Mitch

---
+++ src/views/Mitch/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Layout } from '../../components/Layout/';
+import Box from '@material-ui/core/Box';
+import { Typography } from '@material-ui/core';
+
+
+
+const Mitch = () => {
+    return(
+        <Layout>
+            <Box >
+                <Typography variant="h2">
+                    Mitch
+
+                </Typography>
+            </Box>
+
+        </Layout>
+
+    )
+}
+export default Mitch;
